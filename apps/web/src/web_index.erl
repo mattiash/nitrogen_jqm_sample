b4a5a156944e3af3b7d23a262fba52fc4e2f6d4d
Mattias Holmlund <mattias@holmlund.se>
2011-10-02 13:48:11 +0200

    Turn page1 into simple form demo.

--- apps/web/src/web_index.erl
+++ apps/web/src/web_index.erl
@@ -9,5 +9,13 @@ title() -> "Nitrogen Web Framework for Erlang".
 
 layout() ->
     [
-     #link { id=page1, url='/page1', text="PAGE 1" }
+     #literal { text="<ul data-role='listview' data-inset='true'>",
+                html_encode=false},
+     #literal { 
+       text="<li data-role='list-divider' role='heading'>List divider</li>",
+       html_encode=false },
+     #literal { text="<li data-theme='c'>", html_encode=false },
+     #link { id=page1, url='/page1', text="Page 1" },
+     #literal { text="</li>", html_encode=false },
+     #literal { text="</ul>", html_encode=false }
     ].
--- apps/web/src/web_page1.erl
+++ apps/web/src/web_page1.erl
@@ -10,10 +10,28 @@ title() -> "Nitrogen Web Framework for Erlang".
 layout() ->
     [
      #h1 { text="My Simple Application" },
-     #label { text="What is your name?" },
-     #textbox { },
-     #button { text="Submit", postback=submit }
-    ].
+     #panel { data_role="fieldcontain", body=[
+       #label { for="f1", text="Text input:" },
+       #literal { text="<input type='text' id='f1' value='' />",
+		  html_encode=false }
+       ] },
 
-event(submit) ->
-    io:format( "Hello~n" ).
+     #panel { data_role="fieldcontain", body=[
+       #label { for="f2", text="Flip switch:" },
+       #dropdown { id=f2, html_id="f2", data_role="slider",
+		   options= [
+                     #option { text="Off", value='off' },
+                     #option { text="On", value='on', selected=true }
+                   ], postback={set, "f2"} },
+ 
+     #panel { data_role="fieldcontain", body=[
+       #label { for="f3", text="Slider:" },
+       #literal { text="<input type='range' id='f3' value='0' min='0' max='100'/>", html_encode=false }
+     ] },
+
+     #button { text="Submit", postback=submit }
+    ] 
+  }].
+
+event(_Arg) ->
+    io:format( "event( ~p )~n", [_Arg] ).
